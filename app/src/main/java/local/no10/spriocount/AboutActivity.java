1f57a10dd122bf1a5fbc6c11e33d1cd82adbe687
djewing <dominicewing@gmail.com>
2021-11-22 12:15:43 -0800

    Add about page

---
+++ app/src/main/java/local/no10/spriocount/AboutActivity.java
@@ -0,0 +1,18 @@
+package local.no10.spriocount;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Empty activity for displaying the about info.
+ */
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
